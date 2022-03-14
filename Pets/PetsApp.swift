b9dd24b9bef21736bdfe26ee0363544d919c57ef
Mohammad Azam <azamsharp@gmail.com>
2022-03-14 13:28:52 -0500

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Mohammad Azam on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
